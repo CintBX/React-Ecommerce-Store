2b0647896e117e398c50fe66e6585462471b4660
Dustin Cintron <D.Cint88@gmail.com>
2019-09-20 11:48:17 -0400

    App, Button, Default, Details, Product, Cart, and Modal components

---
+++ src/components/Cart/CartColumns.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function CartColumns() {
+	return (
+		<div className="container-fluid text-center d-none d-lg-block">
+			<div className="row">
+				<div className="col-10 mx-auto col-lg-2">
+					<h4 className="text-uppercase">products</h4>
+				</div>
+				<div className="col-10 mx-auto col-lg-2">
+					<h4 className="text-uppercase">name of product</h4>
+				</div>
+				<div className="col-10 mx-auto col-lg-2">
+					<h4 className="text-uppercase">price</h4>
+				</div>
+				<div className="col-10 mx-auto col-lg-2">
+					<h4 className="text-uppercase">quantity</h4>
+				</div>
+				<div className="col-10 mx-auto col-lg-2">
+					<h4 className="text-uppercase">remove</h4>
+				</div>
+				<div className="col-10 mx-auto col-lg-2">
+					<h4 className="text-uppercase">total</h4>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CartColumns;
--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 class Default extends Component {
 	render() {
+		console.log(this.props);
 		return (
-			<div>
-				<h3>Sorry! Page cannot be found.</h3>
+			<div className="container">
+				<div className="row">
+					<div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+						<br/>
+						<h1 className="display-3">404</h1>
+						<h1>error</h1><br/>
+						<h3>
+							cannot find the requested url
+							(<span className="text-danger">{this.props.location.pathname}</span>)
+						</h3>
+						<h5>please try again</h5>
+					</div>
+				</div>
 			</div>
 		)
 	}
